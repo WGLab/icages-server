b101d9a525b5782e29d4f51a566cdc956e30105a
Coco Dong <cocodong@bioinform2.med.usc.edu>
2014-07-29 22:06:56 -0700

    added email field

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,5 +1,5 @@
 class UploadController < ApplicationController 
-  protect_from_forgery except: :handle_uplaod
+  protect_from_forgery except: :handle_upload
   before_filter :add_headers, :only => [:handle_upload, :options]
 
   def options
@@ -12,13 +12,17 @@ class UploadController < ApplicationController
     inputData = params[:inputData]
 
     inputFile = params[:inputFile]
+
+    email = params[:email]
     
     logger.debug "Input file is [#{'empty' if inputFile.nil?}].." 
+
+    email_msg = (email.nil? || email.empty?) ? '':'for ' + email
 
     if inputData
       submission = Submission.create(done: false)
       Thread.new { exec_query(submission.id, inputData, {isFile: false}) }
-      render json: {id: submission.id, msg:"Submission: #{submission.id} created"}
+      render json: {id: submission.id, msg:"Submission: #{submission.id} created #{email_msg}"}
     elsif inputFile
       submission = Submission.create(done: false)
       Thread.new { exec_query(submission.id, inputFile, {isFile: true}) }
@@ -31,13 +35,13 @@ class UploadController < ApplicationController
   
   def exec_query(id, data, opts)
     working_dir = Rails.root
-    input_file = '/home/cocodong/project/iCAGES/pipeline/input_old.txt'
+    input_file = '/home/cocodong/project/iCAGES/pipeline/input.txt'
     File.open(input_file,'w') do |file|
       file.write(opts[:isFile] ? data.read : data)
     end
    
     results_dir = working_dir.join('public', 'results')
-    script = '/home/cocodong/project/iCAGES/pipeline_old.pl'
+    script = '/home/cocodong/project/iCAGES/pipeline.pl'
     `perl #{script}; cp /home/cocodong/project/iCAGES/pipeline/result.json #{results_dir}/result-#{id}.json`
     
     submission = Submission.find(id)
