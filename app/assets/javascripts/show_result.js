04a019c2b2fb920c64fa073fb16d40c814afa5e9
Peter <yo_heze@hotmail.com>
2014-08-12 03:27:09 +0000

    Added bar chart to show the gene scores, modified the bubble chart so that genes in the same group are clustered together

--- app/assets/javascripts/show_result.js
+++ app/assets/javascripts/show_result.js
@@ -1,46 +1,62 @@
 (function(id) {
     var width = 960,
-        height = 550;
+        height = 650,
+        padding = 1.5, // separation between same-color nodes
+        clusterPadding = 6, // separation between different-color nodes
+        maxRadius = 12;
 
-    var fill = d3.scale.category10();
+    var n = 200, // total number of nodes
+        m = 10, // number of distinct clusters
+        nodes, node, clusters = {};
+
+    var colorScale = d3.scale.category10()
+        .domain(["black", "red", "blue"])
+        .range(['#5cb85c', '#d62728', '#1f77b4']);
 
     d3.json("../results/result-" + id + ".json", function(error, root) {
+        m = 0
+        nodes = root.children;
+        for (var n in nodes) {
+            if (clusters[nodes[n].color] === undefined) {
+                clusters[nodes[n].color] = null;
+                m++;
+            }
+        }
 
-        var nodes = root.children;
+
+        nodes.map(function(d, i) {
+            d.x = Math.cos(i / m * 2 * Math.PI) * 200 + width / 2 + Math.random();
+            d.y = Math.sin(i / m * 2 * Math.PI) * 200 + height / 2 + Math.random();
+            var r = Math.round(d.value * 60);
+            d.radius = r;
+            if (clusters[d.color] === null || r > clusters[d.color].radius) clusters[d.color] = d;
+            return d;
+        });
+
         var force = d3.layout.force()
-            .charge(-170)
             .nodes(nodes)
             .size([width, height])
+            .gravity(.02)
+            .charge(0)
             .on("tick", tick)
             .start();
 
-        var svg = d3.select("div.home_content").append("svg")
+        var svg = d3.select("#bubbleChart").append("svg")
             .attr("width", width)
             .attr("height", height);
 
-        var node = svg.selectAll(".node")
+        node = svg.selectAll(".node")
             .data(nodes)
             .enter().append("g")
-            .attr("class", "node")
             .attr("transform", function(d) {
                 return "translate(" + d.x + "," + d.y + ")";
             })
-            .call(force.drag)
-            .on("mousedown", function() {
-                d3.event.stopPropagation();
-            });
+            .call(force.drag);
 
         node.append("circle")
-            .attr("r", function(d) {
-                return Math.round(d.value * d.value * 40);
-            })
-            .style("fill", function(d, i) {
-                return fill(i & 3);
-            })
-            .style("stroke", function(d, i) {
-                return d3.rgb(fill(i & 3)).darker(2);
+            .style("fill", function(d) {
+                return colorScale(d.color);
             });
-
 
         node.append("text")
             .attr("dy", ".3em")
@@ -53,27 +69,212 @@
                 return d.name;
             });
 
-        svg.style("opacity", 1e-6)
-            .transition()
-            .duration(1000)
-            .style("opacity", 1);
+        node.selectAll("circle").transition()
+            .duration(750)
+            .delay(function(d, i) {
+                return i * 5;
+            })
+            .attrTween("r", function(d) {
+                var i = d3.interpolate(0, d.radius);
+                return function(t) {
+                    return d.radius = i(t);
+                };
+            });
 
-        d3.select("body")
-            .on("mousedown", mousedown);
+    });
 
-        function tick(e) {
-
-            node.attr("transform", function(d) {
+    function tick(e) {
+        node
+            .each(cluster(10 * e.alpha * e.alpha))
+            .each(collide(.5))
+            .attr("transform", function(d) {
                 return "translate(" + d.x + "," + d.y + ")";
-            });
-        }
+            })
+    }
 
-        function mousedown() {
-            nodes.forEach(function(o, i) {
-                o.x += (Math.random() - .5) * 40;
-                o.y += (Math.random() - .5) * 40;
+    // Move d to be adjacent to the cluster node.
+    function cluster(alpha) {
+        return function(d) {
+            var cluster = clusters[d.color];
+            if (cluster === d) return;
+            var x = d.x - cluster.x,
+                y = d.y - cluster.y,
+                l = Math.sqrt(x * x + y * y),
+                r = d.radius + cluster.radius;
+            if (l != r) {
+                l = (l - r) / l * alpha;
+                d.x -= x *= l;
+                d.y -= y *= l;
+                cluster.x += x;
+                cluster.y += y;
+            }
+        };
+    }
+
+    // Resolves collisions between d and all other circles.
+    function collide(alpha) {
+        var quadtree = d3.geom.quadtree(nodes);
+        return function(d) {
+            var r = d.radius + maxRadius + Math.max(padding, clusterPadding),
+                nx1 = d.x - r,
+                nx2 = d.x + r,
+                ny1 = d.y - r,
+                ny2 = d.y + r;
+            quadtree.visit(function(quad, x1, y1, x2, y2) {
+                if (quad.point && (quad.point !== d)) {
+                    var x = d.x - quad.point.x,
+                        y = d.y - quad.point.y,
+                        l = Math.sqrt(x * x + y * y),
+                        r = d.radius + quad.point.radius + (d.color === quad.point.color ? padding : clusterPadding);
+                    if (l < r) {
+                        l = (l - r) / l * alpha;
+                        d.x -= x *= l;
+                        d.y -= y *= l;
+                        quad.point.x += x;
+                        quad.point.y += y;
+                    }
+                }
+                return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
             });
-            force.resume();
-        }
-    })
+        };
+    }
+
+    d3.json("../results/result-" + id +".json", function(error, result) {
+
+        data = result.children;
+        var numOfTicks = 10;
+        var barLabelWidth = 50;
+        var barLabelPadding = 5;
+        var barHeight = 30;
+        var margin = {
+                top: 80,
+                right: 50,
+                bottom: 30,
+                left: 80
+            },
+            width = 1000 - margin.left - margin.right + barLabelWidth,
+            height = data.length * barHeight - margin.top - margin.bottom;
+
+        var x = d3.scale.linear()
+            .range([0, width - barLabelWidth]);
+
+
+        var y = d3.scale.ordinal()
+            .rangeRoundBands([0, height], .1);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .tickSize(height)
+            .outerTickSize(0)
+            .ticks(numOfTicks, ".2f");
+
+        // var yAxis = d3.svg.axis()
+        //     .scale(y)
+        //     .orient("left");
+
+        var svg = d3.select("#barChart").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+        x.domain([d3.min(data, function(d) {
+            return d.value;
+        }), d3.max(data, function(d) {
+            return d.value;
+        })]).nice();
+
+        y.domain(data.map(function(d) {
+            return d.name;
+        }));
+
+        // xAxis, vertical ticks
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(" + barLabelWidth + ",0)")
+            .call(xAxis);
+
+        // Top labels 
+        svg.append("g")
+            .attr("transform", "translate(" + barLabelWidth + ",0)")
+            .selectAll("text")
+            .data(x.ticks(numOfTicks))
+            .enter().append("text")
+            .attr("x", x)
+            .attr("dx", "-1em")
+            .attr("dy", "-0.5em")
+            .text(function(d) {
+                return d3.format(".2f")(d);
+            });
+
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .attr("transform", "translate(" + (barLabelWidth - barLabelPadding) + ")")
+            .selectAll("a")
+            .data(data)
+            .enter().append("a")
+            .attr("xlink:href", function(d) {
+                return d.url;
+            })
+            .attr("target", "_blank")
+            .append("text")
+            .attr('y', function(d) {
+                return y(d.name);
+            })
+            .attr("dy", "1.35em")
+            .attr("text-anchor", "end")
+            .text(function(d) {
+                return d.name;
+            });
+
+        // bars
+
+        var chart = svg.append("g")
+            .attr("transform", "translate(" + barLabelWidth + ")");
+
+        chart.selectAll("a")
+            .data(data)
+            .enter().append("a")
+            .attr("xlink:href", function(d) {
+                return d.url;
+            })
+            .append("rect")
+            .attr("class", "bar")
+            .attr("y", function(d) {
+                return y(d.name);
+            })
+            .style("fill", function(d) {
+                return colorScale(d.color);
+            })
+            .attr("height", y.rangeBand())
+            .attr("width", function(d) {
+                return x(d.value);
+            });
+
+        chart.selectAll("text")
+            .data(data)
+            .enter().append("text")
+            .attr("x", function(d) {
+                return x(d.value);
+            })
+            .attr("y", function(d) {
+                return y(d.name);
+            })
+            .attr("dy", "1.35em")
+            .attr("dx", "0.5em")
+            .text(function(d) {
+                return d3.format(".3f")(d.value);
+            });
+            
+        // .on('click', function(d) {
+        //     window.location.href = d.url;
+        // });
+    });
+
+
 })(SUBMISSION_ID);
+
