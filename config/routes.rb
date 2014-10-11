c3cffb2130123f062574d525454ef4cd9e33b129
hezzze <hezzze@gmail.com>
2014-10-11 13:25:34 -0700

    manually apply changes from branch cocodev

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
 
-
-  get 'coco/awesome'
-
-  get 'result/show'
-
-  get 'welcome/index'
-
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
