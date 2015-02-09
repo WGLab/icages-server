3c1dd32e507ee5b891f2a322c9e37a232b65ce31
hezzze <hezzze@gmail.com>
2015-02-09 13:16:22 -0500

    ng table test

--- app/assets/javascripts/icages-ng.js
+++ app/assets/javascripts/icages-ng.js
@@ -17,11 +17,58 @@ var icages = angular.module('icages', [])
         };
 
         $scope.rowspan = function(s) {
-        	return s === "mutation" ? 1 : 2;
+            return s === "mutation" ? 1 : 2;
         }
 
         $scope.colspan = function(s) {
-        	return s === "mutation" ? 3 : 1;
+            return s === "mutation" ? 3 : 1;
+        }
+
+        function processDataForTable(data) {
+            var result = [];
+            data.forEach(function(d) {
+                var r = {};
+                r.otherFields = [];
+                r.firstRow = true;
+                r.rowspan
+
+                var muts;
+
+                for (var i in d) {
+                    switch (i) {
+                    	case "url":
+                    		r.url = d[i];
+                    		break;
+                        case "gene":
+                            r.geneName = d[i];
+                            break;
+                        case "mutation":
+                            if (d[i].length > 0) {
+                                muts = d[i];
+                                r.mutation = muts[0];
+                            }
+                            break;
+                        case "children":
+                            r.drugs = d[i];
+                            break;
+                        default:
+                            r.otherFields.push(d[i]);
+                            break;
+                    }
+                }
+                result.push(r);
+                for (var j = 1; j < muts.length; j++) {
+                    result.push({
+                        firstRow: false,
+                        mutation: muts[j],
+                        rowspan: 1
+                    });
+                }
+
+            });
+
+			return result;
+
         }
 
         $http.get("../results/result-" + SUBMISSION_ID + ".json").
@@ -31,16 +78,21 @@ var icages = angular.module('icages', [])
 
 
             $scope.log = data.log;
-            $scope.data = data.output;
+            var gData = data.output;
             if ($scope.data.length > 0) {
 
-            	var hs = Object.keys($scope.data[0]);
-            	$scope.headers = hs.filter(function(h) {
-            		return h !== "url";
-            	});
+                var hs = Object.keys(gData[0]);
+                $scope.headers = hs.filter(function(h) {
+                    return h !== "url";
+                });
 
-                $scope.subHeaders = Object.keys($scope.data[0]["mutation"][0]);
+                $scope.mutationHeaders = Object.keys(gData[0]["mutation"][0]);
             }
+
+
+            $scope.geneData = processDataForTable(gData);
+
+            console.log($scope.geneData);
 
         });
     }]);
