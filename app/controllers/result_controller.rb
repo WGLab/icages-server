73642c232e62d4273335b409c7b286fc388e99b1
hezzze <hezzze@gmail.com>
2015-03-23 21:23:52 -0400

    add an assignment statement

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -2,6 +2,7 @@ class ResultController < ApplicationController
   def show
     @submission = Submission.find(params[:id])
 rescue ActiveRecord::RecordNotFound
+	@submission = nil
 	logger.debug "Record not found for submission id:#{params[:id]}" 
   end
 end
