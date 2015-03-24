3cea7e7f38b726c079a4a0ece6736eea655464c4
hezzze <hezzze@gmail.com>
2015-03-23 21:02:22 -0400

    Add exception handler for and error html for illegal id

--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,5 +1,7 @@
 class ResultController < ApplicationController
   def show
     @submission = Submission.find(params[:id])
+rescue ActiveRecord::RecordNotFound
+	logger.debug "Record not found for submission id:#{params[:id]}" 
   end
 end
