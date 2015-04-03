043b85e6eaeb8e59af6982ec270932c2c24d6c72
hezzze <hezzze@gmail.com>
2015-04-02 22:42:16 -0400

    added missing method for adding cross origin headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   CONFIG = JSON[File.read(File.join(Rails.root, 'icages_config.json'))]
+
+  def add_cross_origin_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 end
